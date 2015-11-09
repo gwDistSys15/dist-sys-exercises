7455ab4b50a4a8e53ee4ab83c1db10b0e4805ed9
EC2 Default User <ec2-user@ip-10-235-78-197.ec2.internal>
2015-11-09 20:16:51 +0000

    python wordcount

---
+++ lec-10/hadoop/reduce.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+#  WordCount reducer in Python
+import sys
+
+word_list = {}
+
+## collect (key,val) pairs from sort phase
+for line in sys.stdin:
+    try:
+        word, count = line.strip().split("\t", 2)
+        
+        if word not in word_list:
+            word_list[word] = int(count)
+        else:
+            word_list[word] += int(count)
+    except ValueError, err:
+        sys.stderr.write("Value ERROR: %(err)s\n%(data)s\n" % {"err": str(err), "data": line})
+## emit results
+for word, count in word_list.items():
+    print " ".join([word, str(count)])
+
